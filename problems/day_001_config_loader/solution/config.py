b892045aa69b378d558bad4d30d7c2048464da0a
Muhammad Farhan <malikf355553@gmail.com>
2026-02-01 17:28:32 +0500

    updated config.py with logger

--- problems/day_001_config_loader/solution/config.py
+++ problems/day_001_config_loader/solution/config.py
@@ -1,4 +1,12 @@
 import yaml
+import logging
+from logger import setuplogger
+
+
+setuplogger()
+
+logger = logging.getLogger(__name__)
+
 
 class config():
     DB : str = ""
@@ -7,19 +15,33 @@ class config():
     Timeout : int = 15
 
     def load(self , envType):
+
+        logger.info("Loading Configuration for environment : %s",envType)
+
+        try:
+           with open(f'config/{envType}.yaml','r') as file:
+               configdata = yaml.safe_load(file)
+        except FileNotFoundError:
+            logger.critical("configuration file not found for envionment : %s",envType)
+            raise
+        except yaml.YAMLError as error:
+            logger.critical("cannot parse file : %s", error)
+            raise
+
+        if 'db' not in configdata:
+            logger.critical("missing 'db' section in configuration file ")
+            raise ValueError("Invalid Configuration Structure")
         
-        #read from yaml file 
-        with open(f'config/{envType}.yaml' ,'r') as file:
-            
-            configdata = yaml.safe_load(file) # safe parsing into py dictionaires
+        dbconfig = configdata['db']
+        logger.debug("configuration keys : %s", list(dbconfig.keys()))
 
-            #define config type
-            dbconfig = configdata['db']
+        self.DB = dbconfig.get("DB","")
+        self.TableName = dbconfig.get("TableName","")
+        self.Password = dbconfig.get("Password","")
+        self.Timeout = dbconfig.get("Timeout", 15)
 
-            #assign data
-            self.DB = dbconfig.get('DB','')
-            self.TableName = dbconfig.get('TableName','')
-            self.Password = dbconfig.get('Password','')
-            self.Timeout = dbconfig.get('Timeout',15)
+        logger.info("Configuration Loaded Successfully for environment : %s  (DB = %s , TableName = %s , Timeout = %s)", envType,self.DB,self.TableName,self.Timeout)
+        
+        return self
 
-            return self
+     
--- problems/day_001_config_loader/solution/main.py
+++ problems/day_001_config_loader/solution/main.py
@@ -1,9 +1,9 @@
 from config import config
 
 
-cfg = config().load("dev")
+cfg = config().load('stag')
 
-
+print("")
 print(cfg.DB)
 print(cfg.TableName)
 print(cfg.Password)
